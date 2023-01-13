f0e41a91086f248104dc8c5422e0a8b26a129735
Fran Zekan <zekan.fran369@gmail.com>
2023-01-13 09:59:41 +0100

    Whole story + script

--- run.sh
+++ run.sh
@@ -7,13 +7,23 @@ ARRAY=(baka crvenkapa lovac panda vuk zec)
 # openssl x509 -pubkey -noout -in ./data/baka.crt > ./data/baka.pem
 # openssl x509 -pubkey -noout -in ./data/vuk.crt > ./data/vuk.pem
 
+rm ./story.txt
+
 for person in "${ARRAY[@]}"
 do
-    # some people are missing private keys derive them from the certificate
+    # some people are missing public keys derive them from the certificate
     if [ ! -f ./data/$person.pem ]; then
         openssl x509 -pubkey -noout -in ./data/$person.crt > ./data/$person.pem
-    fi
+    elif [ -f ./data/$person.crt ]; then
+        # check if the public key is valid and matches the certificate
+        openssl x509 -pubkey -noout -in ./data/$person.crt > ./data/$person.pem.tmp
 
+        if ! diff ./data/$person.pem ./data/$person.pem.tmp > /dev/null ; then
+            echo "!!!Public key does not match certificate!!!"
+        fi
+
+        rm ./data/$person.pem.tmp
+    fi
 
     echo "---------------"
     echo "Testing $person"
@@ -25,9 +35,27 @@ do
         echo "!!!Signature is invalid!!!"
     fi
 
+    if [ ! -f ./data/$person.crt ]; then
+        echo "[[[[Certificate is missing]]]]"
+        continue
+    fi
+
     cert_valid="$(openssl verify -CAfile ./data/ca.crt ./data/$person.crt)"
     echo "Certificate is valid: $cert_valid"
 
-    expired="$(openssl x509 -checkend 0 -noout -in ./data/$person.crt)"
-    echo "Certificate is expired: $expired"
+    username=$person
+    if [ $person = "crvenkapa" ]; then # name in certificate is rrrh
+        username="rrrh"
+    fi
+
+    if ! grep -q $username ./data/$person.crt; then
+        echo "!!!Person name is not in the certificate!!!"
+    fi
+
+    echo "$person\n" >> ./story.txt
+    cat ./data/$person.txt >> ./story.txt
+    echo "\n" >> ./story.txt
+
+    # expired="$(openssl x509 -checkend 0 -in ./data/$person.crt)"
+    # echo "Certificate is expired: $expired"
 done
